88ec3c469b36f1c2afb88fab72ef459a9cf8d4a9
evan bechtol <evanbechtol@gmail.com>
2021-09-17 08:42:03 -0500

    Fix error caused by not using key on feed

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,37 +1,36 @@
 import "../../assets/css/layout.css";
-import "./Feed.css"
+import "./Feed.css";
 import Post from "../Post/Post";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import httpService from "../../services/httpService";
 
-const Feed = (props) => {
-    const [refreshFeed, setRefreshFeed] = useState(true);
-    const [posts, setPosts] = useState([]);
+const Feed = ( props ) => {
+  const [ refreshFeed, setRefreshFeed ] = useState( true );
+  const [ posts, setPosts ] = useState( [] );
 
-    useEffect(() => {
-        httpService.get("https://jsonplaceholder.typicode.com/posts")
-            .then(response => {
-                setPosts(response.data);
-                console.log(response.data);
-            })
-            .catch(err => {
-                console.error(err);
-            });
-    }, [refreshFeed]);
+  useEffect( () => {
+    httpService.get( "https://jsonplaceholder.typicode.com/posts" )
+      .then( response => {
+        setPosts( response.data );
+      } )
+      .catch( err => {
+        console.error( err );
+      } );
+  }, [ refreshFeed ] );
 
-    return (
-        <div className="feed__container justify-center align-content-center">
-            {
-                posts.map(post => {
-                    return (
-                        <div className="feed__post">
-                            <Post post={post}/>
-                        </div>
-                    )
-                })
-            }
-        </div>
-    );
+  return (
+    <div className="feed__container justify-center align-content-center">
+      {
+        posts.map( post => {
+          return (
+            <div key={post.id} className="feed__post">
+              <Post post={post}/>
+            </div>
+          );
+        } )
+      }
+    </div>
+  );
 };
 
 export default Feed;
