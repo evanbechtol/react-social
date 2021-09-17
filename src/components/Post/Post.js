5b56798fa16c410dcad672c0940219008c90ba8d
evan bechtol <evanbechtol@gmail.com>
2021-09-17 08:48:31 -0500

    Update post styles and avatar

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,34 +1,40 @@
 import "../../assets/css/layout.css";
 import "./Post.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEllipsisH, faThumbsUp } from "@fortawesome/free-solid-svg-icons";
+import {
+  faEllipsisH,
+  faThumbsUp,
+  faUserCircle
+} from "@fortawesome/free-solid-svg-icons";
 
-const Post = ({post}) => {
+const Post = ( { post } ) => {
   return (
-      <div className="post__container">
-          <div className="row flex-container post__header">
-              <div className="post__header-title">
-                  <div className="post__avatar"/>
-                  <p className="post__title">{post.title}</p>
-              </div>
-
-              <div className="post__actions">
-                  <button id={`actions-button-post-${post.id}`} aria-label="Actions">
-                    <FontAwesomeIcon icon={faEllipsisH} />
-                  </button>
-              </div>
+    <div className="post__container">
+      <div className="row flex-container post__header">
+        <div className="post__header-title">
+          <div className="post__avatar">
+            <FontAwesomeIcon icon={faUserCircle} size="3x"/>
           </div>
+          <p className="post__title">{post.title}</p>
+        </div>
 
-          <div className="row">
-              <div className="post__content">{post.body}</div>
-          </div>
-
-          <div className="row post_reactions">
-              <button id={`like-post-${post.id}`} aria-label="Like">
-                <FontAwesomeIcon icon={faThumbsUp} size="lg" />
-              </button>
-          </div>
+        <div className="post__actions">
+          <button id={`actions-button-post-${post.id}`} aria-label="Actions">
+            <FontAwesomeIcon icon={faEllipsisH}/>
+          </button>
+        </div>
       </div>
+
+      <div className="row">
+        <div className="post__content">{post.body}</div>
+      </div>
+
+      <div className="row post_reactions">
+        <button id={`like-post-${post.id}`} aria-label="Like">
+          <FontAwesomeIcon icon={faThumbsUp} size="lg"/>
+        </button>
+      </div>
+    </div>
   );
 };
 
