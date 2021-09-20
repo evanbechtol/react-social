546fb80cd17bbc48b3fe8651b4b9999e8ff86d40
Evan Bechtol <evanbechtol@gmail.com>
2021-09-20 10:49:51 -0500

    Update Feed.js to include better examples of conditional rendering

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,36 +1,50 @@
 import "../../assets/css/layout.css";
-import "./Feed.css";
+import "./Feed.css"
 import Post from "../Post/Post";
-import { useEffect, useState } from "react";
+import {useEffect, useState} from "react";
 import httpService from "../../services/httpService";
 
-const Feed = ( props ) => {
-  const [ refreshFeed, setRefreshFeed ] = useState( true );
-  const [ posts, setPosts ] = useState( [] );
+const Feed = (props) => {
+    const [refreshFeed, setRefreshFeed] = useState(true);
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
-  useEffect( () => {
-    httpService.get( "https://jsonplaceholder.typicode.com/posts" )
-      .then( response => {
-        setPosts( response.data );
-      } )
-      .catch( err => {
-        console.error( err );
-      } );
-  }, [ refreshFeed ] );
+    useEffect(() => {
+        setIsLoading(true);
+        setTimeout(() => {
+            httpService.get("https://jsonplaceholder.typicode.com/posts")
+                .then(response => {
+                    setPosts(response.data);
+                    console.log(response.data);
+                })
+                .catch(err => {
+                    console.error(err);
+                })
+                .finally(() => setIsLoading(false));
+        }, 2000);
+    }, [refreshFeed]);
 
-  return (
-    <div className="feed__container justify-center align-content-center">
-      {
-        posts.map( post => {
-          return (
-            <div key={post.id} className="feed__post">
-              <Post post={post}/>
-            </div>
-          );
-        } )
-      }
-    </div>
-  );
+    const ListOfPosts = () => {
+        return !posts.length
+            ? "No posts to display"
+            : posts.map(post => {
+                return (
+                    <div key={post.id} className="feed__post">
+                        <Post post={post}/>
+                    </div>
+                )
+            });
+    };
+
+    return (
+        <div className="feed__container justify-center align-content-center">
+            {
+                isLoading
+                    ? "Loading...."
+                    : <ListOfPosts/>
+            }
+        </div>
+    );
 };
 
 export default Feed;
