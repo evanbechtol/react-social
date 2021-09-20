ba5d36df80dd41c82ace011b01960aaeb8c758cb
Evan Bechtol <evanbechtol@gmail.com>
2021-09-20 13:02:58 -0500

    Update CreatePost.js
    - Create basic functionality

--- src/components/CreatePost/CreatePost.js
+++ src/components/CreatePost/CreatePost.js
@@ -1,7 +1,23 @@
 import "./CreatePost.css";
+import {useState} from "react";
 
 const CreatePost = (props) => {
-  return (<></>);
+    const [enteredPost, setEnteredPost] = useState("");
+
+    const handleInputChanged = (e) => {
+      setEnteredPost(e.target.value);
+    };
+
+    const handleCreatePost = () => {
+        props.onCreatePost(enteredPost);
+    };
+
+    return (
+        <div className="create-post__container">
+            <input placeholder="Start typing..." value={enteredPost} onChange={handleInputChanged}/>
+            <button onClick={handleCreatePost}>Post</button>
+        </div>
+    );
 };
 
 export default CreatePost;
