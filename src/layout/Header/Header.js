d957c436aebe2a9c2a06c5de63cab0429820adb5
evan bechtol <evanbechtol@gmail.com>
2021-09-23 08:57:19 -0500

    Remove unused references from Header.js

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,8 +1,5 @@
 import "../../assets/css/layout.css";
 import "./Header.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faUser } from "@fortawesome/free-solid-svg-icons";
-import { useEffect, useRef } from "react";
 
 const Header = ( props ) => {
 
