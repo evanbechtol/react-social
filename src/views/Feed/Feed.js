7065c6b8db17cbda20f913135307569eb1ebced0
evan bechtol <evanbechtol@gmail.com>
2021-09-23 08:56:59 -0500

    Remove unused references from Feed.js

--- src/views/Feed/Feed.js
+++ src/views/Feed/Feed.js
@@ -1,10 +1,5 @@
 import "./Feed.css";
 import "../../assets/css/layout.css";
-import CreatePost from "../../components/CreatePost/CreatePost";
-import FeedList from "../../components/FeedList/FeedList";
-import {useEffect, useState} from "react";
-import httpService from "../../services/httpService";
-import {v4 as uuidv4} from 'uuid';
 
 const Feed = () => {
 
