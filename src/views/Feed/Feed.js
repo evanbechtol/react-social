2afb61978be9519ec1932dcac7676108560e18ec
evan bechtol <evanbechtol@gmail.com>
2022-03-14 11:52:56 -0500

    Refactor Feed.js to improve how state was being updated

--- src/views/Feed/Feed.js
+++ src/views/Feed/Feed.js
@@ -2,9 +2,9 @@ import "./Feed.css";
 import "../../assets/css/layout.css";
 import CreatePost from "../../components/CreatePost/CreatePost";
 import FeedList from "../../components/FeedList/FeedList";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import httpService from "../../services/httpService";
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
 const Feed = () => {
     const [posts, setPosts] = useState([]);
@@ -28,7 +28,7 @@ const Feed = () => {
 
     const handleCreatePost = (e) => {
         const newPost = {id: uuidv4(), userId: 1, title: "Some Title", body: e};
-        setPosts([newPost].concat(posts));
+        setPosts(prevState => [newPost, ...prevState]);
     };
 
     return (
